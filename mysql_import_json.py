af2b8452eb0fd343b0882d6f99dbf17eec117f00
groth00 <busygroth@gmail.com>
2021-03-30 19:53:44 -0700

    Add files via upload

--- mysql_import_json.py
+++ mysql_import_json.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 30 19:25:57 2021
+
+@author: groth
+"""
 
 import pandas as pd
 from mysql.connector import connect, Error
 from getpass import getpass
 
 b = pd.read_json('yelp_business_restaurants.json')
+flattened_col = pd.DataFrame(
+[(index, value) for (index, values) in b.categories.iteritems() \
+ for value in values], columns=['index', 'categories']).set_index('index')
+temp = b.drop(['name', 'address', 'city', 'state', 'postal_code',
+               'latitude', 'longitude', 'stars', 'review_count',
+               'categories'], axis = 1).join(flattened_col)
 b = b.drop('categories', axis = 1)
-
-b_denorm = pd.read_json('yelp_business_denormalized.json.gz', 
-                        compression = 'gzip')
 
 def width(df, attribute):
     return max([len(x) for x in df[attribute]])
@@ -19,7 +27,7 @@ widths = [width(b, 'business_id'),
           width(b, 'address'),
           width(b, 'city'),
           width(b, 'state'),
-          width(b, 'postal_code')]
+          width(b, 'postal_code'),]
 
 try:
     connection = connect(host="localhost", 
@@ -37,6 +45,14 @@ except Error as e:
 try:
     with connection.cursor() as cursor:
         cursor.execute('use yelp')
+except Error as e:
+    print(e)
+    
+try:
+    with connection.cursor() as cursor:
+        cursor.execute('drop table if exists business')
+        cursor.execute('drop table if exists business_denormalized')
+        cursor.execute('drop table if exists categories')
 except Error as e:
     print(e)
 
@@ -62,34 +78,20 @@ try:
 except Error as e:
     print(e)
 
-# check that the schema is correct
-# try:
-#     with connection.cursor() as cursor:
-#         cursor.execute('describe business')
-#         result = cursor.fetchall()
-#         for r in result:
-#             print(r)
-# except Error as e:
-#     print(e)
-
-create_denormalized_table = '''
-create table business_denormalized like business
-'''
-
-add_categories_column = '''
-alter table business_denormalized 
-add column category varchar({})
-'''.format(max([len(x) for x in b_denorm.categories]))
+create_categories_table = '''
+create table categories(
+business_id char({}),
+category varchar({})
+)
+'''.format(widths[0], max([len(x) for x in flattened_col.categories]))
 
 try:
     with connection.cursor() as cursor:
-        cursor.execute(create_denormalized_table)
-        cursor.execute(add_categories_column)
+        cursor.execute(create_categories_table)
         connection.commit()
 except Error as e:
     print(e)
     
-
 # show tables
 try:
     with connection.cursor() as cursor:
@@ -100,9 +102,8 @@ try:
 except Error as e:
     print(e)
 
-
 business_values = [row for row in b.itertuples(index=False, name=None)]
-denormalized_values = [row for row in b_denorm.itertuples(index=False, name=None)]
+category_values = [row for row in temp.itertuples(index=False, name=None)]
 
 insert_into_business = '''
 insert into business
@@ -111,16 +112,15 @@ insert into business
 values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
 '''
 
-insert_into_denormalized = '''
-insert into business_denormalized
-(business_id, name, address, city, state, postal_code,
- latitude, longitude, stars, review_count, category)
-values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+insert_into_categories = '''
+insert into categories
+(business_id, category)
+values(%s, %s)
 '''
 
 with connection.cursor() as cursor:
     cursor.executemany(insert_into_business, business_values)
-    cursor.executemany(insert_into_denormalized, denormalized_values)
+    cursor.executemany(insert_into_categories, category_values)
     connection.commit()
 
 # check that all rows were entered: 63944 and 274306
@@ -129,12 +129,43 @@ with connection.cursor() as cursor:
     result = cursor.fetchall()
     for r in result:
         print(r)
-
+        
 with connection.cursor() as cursor:
-    cursor.execute('select count(*) from business_denormalized')
+    cursor.execute('select count(*) from categories')
     result = cursor.fetchall()
     for r in result:
         print(r)
+
+# some queries
+businesses_per_state = '''
+select state, count(*) as num_restaurants
+from business
+group by state
+order by num_restaurants desc;
+'''
+with connection.cursor() as cursor:
+    cursor.execute(businesses_per_state)
+    result = cursor.fetchall()
+    for r in result:
+        print(r)
+
+''' groups businesses by category and returns avg star rating
+for group size > 50
+'''
+
+avg_stars_query = '''
+select category, avg(stars)
+from business join categories using(business_id)
+group by category
+having count(*) > 50
+'''
+with connection.cursor() as cursor:
+    cursor.execute(avg_stars_query)
+    result = cursor.fetchall()
+    for r in result:
+        print(r)
+
+
 
 connection.close()
 
